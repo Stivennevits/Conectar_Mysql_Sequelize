52ae2f6fb639e278d0597615d691cdd06c0d1f7a
stivennevits <stivennevits16@gmail.com>
2023-02-15 12:31:32 -0500

    controller movies ok

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,4 +1,5 @@
 const db = require("../database/models")
+const sequelize = db.sequelize
 
 const moviesController = {
     list: /*(req, res) =>{
@@ -16,8 +17,25 @@ const moviesController = {
         res.render("moviesDetail", {movie})
     },
     new: async(req,res) => {
-        const movies = await db.Movie.findAll()
-    }      
+        const movies = await db.Movie.findAll({
+            order: [
+                ["release_date", "DESC"]
+            ],
+            limit: 5
+        })
+        res.render("newestMovies", {movies})
+    },
+    recomended : async(req,res) => {
+        const movies = await db.Movie.findAll({
+            where:{
+                rating: {[db.Sequelize.Op.gt]: 8}
+            },
+            order: [
+                ["rating", "DESC"]
+            ]
+        })
+        res.render("recommendedMovies", {movies})
+    }       
 }
 
 
